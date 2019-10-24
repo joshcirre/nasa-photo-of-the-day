9453a79531090f2a6f2960e3b5cf4b7e2c34d09b
Josh Cirre <joshcirre@gmail.com>
2019-10-24 16:51:44 -0700

    changed styling for datepicker buttons

--- src/Components/NextPrevButtons.js
+++ src/Components/NextPrevButtons.js
@@ -2,8 +2,31 @@ import React from 'react'
 import styled from '@emotion/styled'
 
 const PrevNextButton = styled.button`
-  margin: 1rem;
-  padding: 0.2rem;
+    display: inline-block;
+    border: none;
+    padding: 1rem 2rem;
+    margin: 0;
+    text-decoration: none;
+    background: #0069ed;
+    color: #ffffff;
+    font-family: sans-serif;
+    font-size: 1rem;
+    cursor: pointer;
+    text-align: center;
+    transition: background 250ms ease-in-out, 
+    transform 150ms ease;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    &:hover, &:focus {
+        background: #0053ba;
+    }
+    &:focus {
+        outline: 1px solid #fff;
+        outline-offset: -4px;
+    }
+    &:active {
+        transform: scale(0.99);
+    }
 `
 
 export default function(props) {
